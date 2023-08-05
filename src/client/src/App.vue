4bd6a420cbc1279df75bce16fc9e2d0292d63e5e
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-08-05 22:19:51 +0300

    changed location of login form

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,6 +1,28 @@
 <template>
+  <nav
+    aria-label="Navigation"
+    class="xt-list xt-list-2 mx-auto flex max-w-7xl items-center justify-between mb-10 container"
+  >
+    <div class="flex">
+      <h2>
+        La Pasticciosa
+        <div
+          class="inline-block align-middle py-3 px-3.5 text-xs rounded-xs text-gray-900 font-medium leading-snug tracking-wider uppercase bg-gray-100"
+        >
+          B2B Portal
+        </div>
+      </h2>
+    </div>
+    <div class="flex">
+      <button
+        class="xt-button rounded-full border-4 p-2 border-slate-300 hover:border-primary-300 w-12 h-12"
+      >
+        <i class="h h-power text-2xl"></i>
+      </button>
+    </div>
+  </nav>
   <div class="container">
-    <HomeView />
+    <RouterView />
   </div>
 </template>
 
