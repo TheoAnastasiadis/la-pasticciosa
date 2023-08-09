ba051a051669d2d4484dca51c3ce69cfa8290fd0
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-08-10 00:14:23 +0300

    made the header into its own component

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,32 +1,5 @@
 <template>
-  <nav
-    aria-label="Navigation"
-    class="xt-list xt-list-2 mx-auto w-11/12 flex max-w-10xl items-center justify-between mb-5 md:mb-10 container"
-  >
-    <div class="flex items-baseline">
-      <h2 class="text-xl font-bold md:text-2xl">
-        La Pasticciosa
-        <div
-          class="align-middle py-3 px-3.5 text-xs rounded-xs text-gray-900 font-medium leading-snug tracking-wider uppercase bg-gray-200 hidden sm:inline-block"
-        >
-          B2B Portal
-        </div>
-      </h2>
-    </div>
-    <div class="flex">
-      <button
-        class="xt-button rounded-full border-1 md:border-2 p-2 border-slate-300 hover:border-primary-300 w-10 h-10 md:w-12 md:h-12 bg-slate-200"
-      >
-        <i
-          class="h text-xl"
-          :class="{
-            'h-user': !this.userStore.user,
-            'h-power': this.userStore.user,
-          }"
-        ></i>
-      </button>
-    </div>
-  </nav>
+  <Header />
   <div class="container">
     <RouterView />
   </div>
@@ -40,12 +13,9 @@ import "xtendui/src/overlay";
 import "xtendui/src/textareaautosize";
 import "xtendui/src/tooltip";
 
-import { mapStores } from "pinia";
-import { useUserStore } from "./stores/user";
+import Header from "./components/header.vue";
 
 export default {
-  computed: {
-    ...mapStores(useUserStore),
-  },
+  components: { Header },
 };
 </script>
