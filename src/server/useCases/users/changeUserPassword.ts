81c4230f5a63c5ae4ecd694fe5546cc85bca8141
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-08-11 21:24:54 +0300

    fixes and TypeOrm bugs

--- src/server/useCases/users/changeUserPassword.ts
+++ src/server/useCases/users/changeUserPassword.ts
@@ -5,5 +5,8 @@ export const changeUserPassword: (
   newPassword: string,
 ) => Promise<User> = async (user, newPassword) => {
   user.password = newPassword;
+  // @ts-expect-error (When an item in user.catalogue is in an altered state (example: deleted), typeorm will try to re insert they user_uuid <-> item_id row in the join table, throwing a unique contraint error.)
+  user.catalogue = user.catalogue.map((item) => item.id);
+  console.log(JSON.stringify(user, undefined, 2));
   return await user.save();
 };
