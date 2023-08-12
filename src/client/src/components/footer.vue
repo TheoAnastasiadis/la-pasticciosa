a22c4c54b755b86b8eaad7357d63fc42a28411da
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-08-12 13:46:22 +0300

    changed navigation position on mobile devices

--- src/client/src/components/footer.vue
+++ src/client/src/components/footer.vue
@@ -1,15 +1,23 @@
 <template>
   <div
-    class="w-full h-10 flex flex-row items-baseline justify-end space-x-10 py-5 px-10 absolute bottom-5"
+    class="w-full min-h-fit flex flex-col md:flex-row items-baseline justify-center d:justify-between py-4 px-10 absolute bottom-14 md:bottom-0 text-sm"
   >
-    <span class="text-slate-400 mr-auto"
-      >&copy; La Pasticciosa {{ new Date().getFullYear() }}</span
-    >
-    <a href="#" class="text-slate-400 hover:text-slate-600"
-      >Πολιτική απορρήτου</a
-    >
-    <a href="#" class="text-slate-400 hover:text-slate-600"
-      >Επεξεργασία προσωπικών δεδομένων και GDPR</a
-    >
+    <div class="w-full md:w-9/12 flex justify-center md:justify-start">
+      <span class="text-slate-400 mb-4 md:mb-0"
+        >&copy; La Pasticciosa {{ new Date().getFullYear() }}</span
+      >
+    </div>
+    <div class="w-full md:w-3/12 flex justify-items-end flex-col md:flex-row">
+      <a
+        href="#"
+        class="text-slate-400 hover:text-slate-600 md:ml-auto text-center mb-2 md:mb-0"
+        >Πολιτική Απορρήτου</a
+      >
+      <a
+        href="#"
+        class="text-slate-400 hover:text-slate-600 md:ml-auto text-center"
+        >Προσωπικά Δεδομένα</a
+      >
+    </div>
   </div>
 </template>
--- src/client/src/components/reusables/top-level/navigation.vue
+++ src/client/src/components/reusables/top-level/navigation.vue
@@ -3,14 +3,30 @@
     <div data-xt-toggle="{ min: 1, duration: 500 }">
       <div class="xt-row xt-row-4 md:xt-row-8">
         <!-- Drawer -->
-        <div class="w-full md:w-3/12">
+        <div
+          class="w-full md:w-3/12 order-last md:order-first fixed z-above bottom-2 right-2 md:static"
+        >
           <div
             class="xt-card p-2 rounded-2xl text-gray-900 xt-links-default bg-white bg-opacity-80 backdrop-filter backdrop-blur-sm backdrop-saturate-50"
           >
             <nav aria-label="Navigation" class="xt-list xt-list-1 flex-col">
               <a
                 role="button"
-                class="xt-button flex-auto py-1.5 px-3 text-sm rounded-2xl font-medium leading-snug justify-start text-left transition hover:bg-white hover:text-primary-500 active:text-white active:bg-primary-500 on:text-white on:bg-primary-500"
+                class="xt-button flex-auto justify-between py-1.5 px-3 text-sm rounded-2xl font-medium leading-snug text-left transition hover:bg-white hover:text-primary-500 active:text-white active:bg-primary-500 on:text-white on:bg-primary-500 bg-opacity-40 md:hidden"
+                @click="open = !open"
+              >
+                <span>Επιλογές</span>
+                <i
+                  class="h text-sm mr-2"
+                  :class="{ 'h-chevrons-down': open, 'h-chevrons-up': !open }"
+                ></i>
+              </a>
+              <a
+                role="button"
+                class="xt-button flex-auto text-sm rounded-2xl font-medium leading-snug justify-start text-left transition-all hover:bg-white hover:text-primary-500 active:text-white active:bg-primary-500 on:text-white on:bg-primary-500 h-0 overflow-hidden p-0 m-0 scale-y-0"
+                :class="{
+                  'py-1.5 px-3 h-auto mt-1 ml-1 transform-none': open,
+                }"
                 data-xt-toggle-element
                 v-for="item in header"
                 :data-xt-hash="(item as any).hash"
@@ -47,5 +63,8 @@
 <script lang="ts">
 export default {
   props: { header: Array },
+  data: () => ({
+    open: true,
+  }),
 };
 </script>
