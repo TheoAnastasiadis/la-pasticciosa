2a35297f99403606a45a13e8b8b3a44461b62024
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-08-16 21:44:47 +0300

    viewDeliveries route should return rich user information

--- src/server/controllers/delivery/viewDeliveries.controller.ts
+++ src/server/controllers/delivery/viewDeliveries.controller.ts
@@ -1,11 +1,6 @@
-import { type z } from "zod";
-import { type delivery } from "../../entities/decoders/delivery.decoder";
 import { viewDeliveries } from "../../useCases/delivery/viewDeliveries";
 import { throwDBError } from "../errors/db.error";
 
-export const viewDeliveriesController: () => Promise<
-  Array<z.infer<typeof delivery>>
-> = async () => {
-  const deliveries = await viewDeliveries().catch(throwDBError);
-  return deliveries.map((d) => d.toSafeOutput());
+export const viewDeliveriesController: () => Promise<any[]> = async () => {
+  return await viewDeliveries().catch(throwDBError);
 };
--- src/server/router/routeAddapters/delivery/viewDeliveries.route.ts
+++ src/server/router/routeAddapters/delivery/viewDeliveries.route.ts
@@ -1,10 +1,19 @@
 import { viewDeliveriesController } from "../../../controllers/delivery/viewDeliveries.controller";
 import { delivery } from "../../../entities/decoders/delivery.decoder";
+import { user } from "../../../entities/decoders/user.decoder";
 import { adminOnlyRoute } from "../../middlewareAddapters/adminRequest";
 import { z } from "zod";
 
 export const viewDeliveriesRoute = adminOnlyRoute
-  .output(z.array(delivery))
+  .output(
+    z.array(
+      delivery
+        .omit({ user: true })
+        .merge(
+          z.object({ user: user.omit({ password: true, catalogue: true }) }),
+        ),
+    ),
+  )
   .query(async () => {
     return await viewDeliveriesController();
   });
