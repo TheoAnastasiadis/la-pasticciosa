9d032560b3786747b219bcc59e35ed3ba97e1841
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-08-29 16:26:09 +0300

    finalizing backend tests

---
+++ test/server/order.spec.ts
@@ -0,0 +1,225 @@
+import { Session } from "../../src/server/entities/session";
+import { AppDataSource } from "../../src/server/database/dataSource";
+import { User, UserType } from "../../src/server/entities/user";
+import { Item } from "../../src/server/entities/item";
+import { Delivery, DeliveryStatus } from "../../src/server/entities/delivery";
+import { appRouter } from "../../src/server/router";
+import { Order, OrderStatus } from "../../src/server/entities/order";
+import { In } from "typeorm";
+import moment from "moment";
+import { Quantity } from "../../src/server/entities/quantity";
+
+describe("Order Entity Use Cases", () => {
+  let adminSessionId: string;
+  let admin: User;
+  let userSessionId: string;
+  let user: User;
+  beforeAll(async () => {
+    await AppDataSource.initialize();
+
+    // create two example users, one of each kind.
+    admin = User.create({
+      email: "admin@email.com",
+      type: UserType.ADMIN,
+      password: "veryStrongPassword",
+      userName: "admin" + Math.random(),
+      companyName: "Company",
+      companyAddress: "Address",
+      vat: "123456789",
+    });
+    await admin.save();
+
+    user = User.create({
+      email: "user@email.com",
+      type: UserType.USER,
+      password: "veryStrongPassword",
+      userName: "user" + Math.random(),
+      companyName: "Company",
+      companyAddress: "Address",
+      vat: "123456789",
+    });
+    await user.save();
+
+    // create sessionIds for each
+    const adminSession = Session.create();
+    adminSession.user = admin;
+    await adminSession.save({ reload: true });
+    adminSessionId = adminSession.id;
+
+    const userSession = Session.create();
+    userSession.user = user;
+    await userSession.save({ reload: true });
+    userSessionId = userSession.id;
+  }, 20000);
+
+  test("place order (as user)", async () => {
+    // create example item and assign to user
+    const item = await Item.save(
+      Item.create({
+        name: "Example Item 1",
+        description: "Lorem Ipsum",
+        price: "10.99",
+        unit: "Kg",
+        image: "https://www.example.com/images/full.png",
+        thumbnail: "https://www.example.com/images/thumb.jpg",
+      }),
+    );
+    user.catalogue = [item];
+    await user.save();
+
+    // create example delivery and assign to user
+    const delivery = await Delivery.save(
+      Delivery.create({
+        name: "Example Delivery 1",
+        user,
+        street: "Example Str.",
+        number: "2A",
+        zip: "12345",
+        details: "Lorem Ipsum",
+        state: DeliveryStatus.ACCEPTED,
+      }),
+    );
+
+    const order = await appRouter
+      .createCaller({
+        sessionId: userSessionId,
+        setCookie: {} as any,
+      })
+      .placeOrder({
+        deliveryId: delivery.id,
+        quantities: [{ item: item.id, value: 1 }],
+      });
+
+    expect(order).toHaveProperty("status", OrderStatus.PENDING);
+    expect(moment(order.estimatedDelivery).unix()).toBe(0);
+    expect(order).toHaveProperty("total", "10.99");
+  }, 20000);
+
+  test("place order (as admin on behalf of user)", async () => {
+    // create example item and assign to user
+    const item = await Item.save(
+      Item.create({
+        name: "Example Item 2",
+        description: "Lorem Ipsum",
+        price: "10.99",
+        unit: "Kg",
+        image: "https://www.example.com/images/full.png",
+        thumbnail: "https://www.example.com/images/thumb.jpg",
+      }),
+    );
+    user.catalogue = [item];
+    await user.save();
+
+    // create example delivery and assign to user
+    const delivery = await Delivery.save(
+      Delivery.create({
+        name: "Example Delivery 2",
+        user,
+        street: "Example Str.",
+        number: "2A",
+        zip: "12345",
+        details: "Lorem Ipsum",
+        state: DeliveryStatus.ACCEPTED,
+      }),
+    );
+
+    const order = await appRouter
+      .createCaller({
+        sessionId: adminSessionId,
+        setCookie: {} as any,
+      })
+      .placeOrder({
+        deliveryId: delivery.id,
+        quantities: [{ item: item.id, value: 1 }],
+        // @ts-expect-error onBehalf is not explicitly set as input for the route
+        onBehalf: user.uuid,
+      });
+
+    expect(order).toHaveProperty("user.uuid", `${user.uuid}`);
+    expect(order).toHaveProperty("status", OrderStatus.PENDING);
+    expect(moment(order.estimatedDelivery).unix()).toBe(0);
+    expect(order).toHaveProperty("total", "10.99");
+  }, 20000);
+
+  test("update status and update estimate", async () => {
+    // create example item and assign to user
+    const item = await Item.save(
+      Item.create({
+        name: "Example Item 3",
+        description: "Lorem Ipsum",
+        price: "10.99",
+        unit: "Kg",
+        image: "https://www.example.com/images/full.png",
+        thumbnail: "https://www.example.com/images/thumb.jpg",
+      }),
+    );
+    user.catalogue = [item];
+    await user.save();
+
+    // create example delivery and assign to user
+    const delivery = await Delivery.save(
+      Delivery.create({
+        name: "Example Delivery 3",
+        user,
+        street: "Example Str.",
+        number: "2A",
+        zip: "12345",
+        details: "Lorem Ipsum",
+        state: DeliveryStatus.ACCEPTED,
+      }),
+    );
+
+    // create example order
+    const order = await appRouter
+      .createCaller({
+        sessionId: adminSessionId,
+        setCookie: {} as any,
+      })
+      .placeOrder({
+        deliveryId: delivery.id,
+        quantities: [{ item: item.id, value: 1 }],
+        // @ts-expect-error onBehalf is not explicitly set as input for the route
+        onBehalf: user.uuid,
+      });
+
+    await appRouter
+      .createCaller({
+        sessionId: adminSessionId,
+        setCookie: {} as any,
+      })
+      .updateOrderStatus({ orderId: order.id, status: OrderStatus.ACCEPTED });
+
+    const orderEntry = await Order.findOneByOrFail({ id: order.id });
+    expect(orderEntry).toHaveProperty("status", OrderStatus.ACCEPTED);
+
+    await appRouter
+      .createCaller({
+        sessionId: adminSessionId,
+        setCookie: {} as any,
+      })
+      .updateOrderEstimate({ id: order.id, day: 2, month: 0, year: 2012 });
+
+    await orderEntry.reload();
+    expect(orderEntry.estimatedDelivery).toEqual("2012-01-01");
+  }, 20000);
+
+  afterAll(async () => {
+    const orders = await Order.find({ where: { user: { uuid: user.uuid } } });
+    await Quantity.delete({
+      order: { id: In(orders.map((order) => order.id)) },
+    });
+    await Order.delete({ id: In(orders.map((order) => order.id)) });
+    await Delivery.delete({
+      name: In([
+        "Example Delivery 1",
+        "Example Delivery 2",
+        "Example Delivery 3",
+      ]),
+    });
+    await Item.delete({
+      name: In(["Example Item 1", "Example Item 2", "Example Item 3"]),
+    });
+    await Session.delete({ id: In([userSessionId, adminSessionId]) });
+    await User.delete({ uuid: In([user.uuid, admin.uuid]) });
+  });
+});
