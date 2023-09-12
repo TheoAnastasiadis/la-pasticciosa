1f6a27d644a8c649dfad2769d5151e17f2423cc3
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-09-12 23:02:21 +0300

    addition: added firebase configuration, and also proxying to augment firebase functions in devlopment

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ import vue from "@vitejs/plugin-vue";
 
 import { babel } from "@rollup/plugin-babel";
 
+import { config } from "dotenv";
+config();
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "./src/client",
+  root: "src/client",
   plugins: [
     (vue as any)(),
     babel({
@@ -18,5 +21,8 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src/client/src", import.meta.url)),
     },
+  },
+  server: {
+    proxy: { "/data": process.env.DEV_DATA_URL as string },
   },
 });
