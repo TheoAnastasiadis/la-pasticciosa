0b6d01399a203fd4702acc02cd9d4ca23f90ff8c
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-09-12 22:57:33 +0300

    change: some functions were not needed

--- src/server/config/app.config.ts
+++ src/server/config/app.config.ts
@@ -2,17 +2,8 @@ import { config } from "dotenv";
 config({ debug: process.env.NODE_ENV === "production" });
 
 export default {
-  getDBUrl: () =>
-    (process.env.NODE_ENV === "production"
-      ? process.env.PRODUCTION_DATABASE_URL
-      : process.env.DEVELOPMENT_DATABASE_URL) as string,
-  getDevPort: () => parseInt(process.env.DEV_PORT ?? "8080"),
-  getServerMountPath: () => process.env.SERVER_MOUNT_PATH ?? "/data",
+  getDBUrl: () => process.env.DATABASE_URL as string,
   getSaltRounds: () => parseInt(process.env.SALT_ROUNDS as string),
-  getClientUrl: () =>
-    (process.env.NODE_ENV === "production"
-      ? process.env.clientUrl
-      : "http://localhost:5173") as string,
+  getClientUrl: () => process.env.CLIENT_URL as string,
   getPaginationLimit: () => 20,
-  getOpenAIKey: () => process.env.OPENAI_API_KEY,
 };
