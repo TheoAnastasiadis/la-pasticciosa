601c42b0a1acf7962965ae8e2bdfe2c2d0849c91
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-09-12 22:58:36 +0300

    change: firebase functions setup

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,47 +1,13 @@
 import "reflect-metadata";
-import express from "express";
-import * as trpcExpress from "@trpc/server/adapters/express";
-import cookieParser from "cookie-parser";
-import { appRouter } from "./router";
-import { createContext } from "./router/setup/context";
-import appConfig from "./config/app.config";
 import { AppDataSource } from "./database";
-import morgan from "morgan";
-import cors from "cors";
+import { onRequest } from "firebase-functions/v2/https";
+import { initializeApp } from "firebase-admin/app";
 
-const app = express();
-const MOUNT_PATH = appConfig.getServerMountPath();
+import dataApp from "./data";
 
-/** Middleware */
-app.use(cookieParser());
-app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: appConfig.getClientUrl(),
-    credentials: true,
-  }),
-);
+/** Init */
+await AppDataSource.initialize();
+initializeApp();
 
-/** Main Router */
-app.use(
-  MOUNT_PATH,
-  trpcExpress.createExpressMiddleware({
-    router: appRouter,
-    createContext,
-  }),
-);
-
-/** Fire server up */
-const PORT = appConfig.getDevPort();
-AppDataSource.initialize()
-  .then(() =>
-    app.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
-    }),
-  )
-  .catch((e) => {
-    console.warn(
-      "Connection with the database could not be established. The server did not start.",
-    );
-    console.error(e);
-  });
+/** Mount */
+export const data = onRequest(dataApp);
