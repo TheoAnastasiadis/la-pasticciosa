662dadf29d95b7f1c8888f3d27d0df8e9224368b
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-09-13 22:50:51 +0300

    addition: new config for google client secrets

--- src/server/config/app.config.ts
+++ src/server/config/app.config.ts
@@ -6,4 +6,6 @@ export default {
   getSaltRounds: () => parseInt(process.env.SALT_ROUNDS as string),
   getClientUrl: () => process.env.CLIENT_URL as string,
   getPaginationLimit: () => 20,
+  getGoogleClientId: () => process.env.GOOGLE_CLIENT_ID as string,
+  getGoogleClientSecret: () => process.env.GOOGLE_CLIENT_SECRET as string,
 };
