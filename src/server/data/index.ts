fed89fb53237684d52af7be6c7b0343ac959031d
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-09-15 13:14:45 +0300

    addition: added sentry and onError middleware

--- src/server/data/index.ts
+++ src/server/data/index.ts
@@ -1,22 +1,26 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import morgan from "morgan";
-import { appRouter } from "./router";
+import { appRouter as router } from "./router";
 import { createContext } from "./router/setup/context";
 import * as trpcExpress from "@trpc/server/adapters/express";
+import onError from "./router/setup/onError";
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
 /** Middleware */
 app.use(cookieParser());
 app.use(morgan("dev"));
+app.use(Sentry.Handlers.requestHandler());
 
 /** Router */
 app.use(
   "/data",
   trpcExpress.createExpressMiddleware({
-    router: appRouter,
+    router,
     createContext,
+    onError,
   }),
 );
 
