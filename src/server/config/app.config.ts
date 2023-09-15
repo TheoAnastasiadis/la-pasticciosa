7884ca1724a93d9b906010147c033462306b9951
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-09-15 13:15:47 +0300

    change: added new functionality for sentry

--- src/server/config/app.config.ts
+++ src/server/config/app.config.ts
@@ -1,11 +1,13 @@
 import { config } from "dotenv";
-config({ debug: process.env.NODE_ENV === "production" });
+config({ debug: process.env.NODE_ENV !== "production" });
 
 export default {
   getDBUrl: () => process.env.DATABASE_URL as string,
+  getDBUsed: () => new URL(process.env.DATABASE_URL as string).pathname,
   getSaltRounds: () => parseInt(process.env.SALT_ROUNDS as string),
   getClientUrl: () => process.env.CLIENT_URL as string,
   getPaginationLimit: () => 20,
   getGoogleClientId: () => process.env.GOOGLE_CLIENT_ID as string,
   getGoogleClientSecret: () => process.env.GOOGLE_CLIENT_SECRET as string,
+  getSentryDsn: () => process.env.SENTRY_DSN as string,
 };
