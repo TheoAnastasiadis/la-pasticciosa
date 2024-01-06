566a795c0488642a8a4206388ae893e23b7f2e01
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2024-01-06 18:56:29 +0200

    fixed minor script errors

--- bin/build.server.sh
+++ bin/build.server.sh
@@ -2,7 +2,7 @@
 rm -rf ./build/
 mkdir build
 tsc --build
-ts-add-js-extension --dir=build
+npx ts-add-js-extension --dir=build
 cp .env ./build/
 cp package.json ./build/
 cp package-lock.json ./build/
--- bin/dev.server.sh
+++ bin/dev.server.sh
@@ -2,5 +2,4 @@
 export NODE_ENV="development"
 npm run build:server
 cp .env ./build/.env
-export VAR="my value"
 firebase emulators:start --only functions
