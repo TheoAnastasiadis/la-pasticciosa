43e5360bc9cf9bac54de1f44bad9ad211f1eedc1
Theo Anastasiadis <theoanastasiadis98@gmail.com>
2024-02-25 19:33:20 +0200

    chore: update src/server/index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,11 @@
 /* istanbul ignore file */
 import "reflect-metadata";
-import { AppDataSource } from "./database";
+import { AppDataSource } from "./database/index";
 import { onRequest } from "firebase-functions/v2/https";
 import { initializeApp } from "firebase-admin/app";
 
-import dataApp from "./data";
-import authApp from "./auth";
+import dataApp from "./data/index";
+import authApp from "./auth/index";
 
 /** Init */
 await AppDataSource.initialize().catch(console.error);
