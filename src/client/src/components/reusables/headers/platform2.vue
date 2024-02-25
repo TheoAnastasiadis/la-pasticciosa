ac683caa072b4e169d75f9d860fac9b70a71f861
Theo Anastasiadis <theoanastasiadis98@gmail.com>
2024-02-25 21:23:40 +0200

    feat(ui): new nav and header

--- src/client/src/components/reusables/headers/platform2.vue
+++ src/client/src/components/reusables/headers/platform2.vue
@@ -1,64 +1,85 @@
 <template>
-  <!-- component -->
-  <header class="bg-white mb-5 shadow-md">
-    <div class="container mx-auto pt-4 flex items-center justify-between">
-      <!-- logo -->
-      <div class="md:w-4/12 xl:w-3/12">
-        <h2 class="text-xl font-quicksand font-bold md:text-2xl tracking-tight">
-          la pasticciosa
-          <div
-            class="align-middle py-2 px-2.5 text-xs rounded-xl text-gray-900 font-medium leading-snug tracking-wider uppercase bg-primary-200 hidden sm:inline-block shadow-sm"
-          >
-            B2B Portal
-          </div>
-        </h2>
-      </div>
-
-      <!-- search -->
+  <header
+    class="z-40 items-center w-[calc(100vw-1rem)] md:w-[calc(100vw-2rem)] mt-4 mb-6 ml-2 md:ml-4 h-16 bg-white shadow-lg dark:bg-gray-700 rounded-2xl"
+  >
+    <div
+      class="relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center"
+    >
       <div
-        class="w-4/12 xl:w-5/12 max-w-xs xl:max-w-lg 2xl:max-w-2xl bg-gray-100 rounded-md hidden lg:flex items-center justify-between"
+        class="flex justify-between items-center w-full pl-1 lg:max-w-68 sm:pr-2 sm:ml-0"
       >
-        <input
-          class="bg-transparent font-semibold text-sm p-4"
-          type="text"
-          :placeholder="`Τι ψάχνεις ${userStore.user?.userName};`"
-        />
-        <svg
-          class="ml-4 h-5 px-4 text-gray-500 svg-inline--fa fa-search fa-w-16 fa-9x"
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="far"
-          data-icon="search"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="currentColor"
-            d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"
-          ></path>
-        </svg>
+        <!-- logo -->
+        <div class="w-4/12">
+          <h2
+            class="w-max md:text-xl font-quicksand font-bold md:text-2xl tracking-tight"
+          >
+            la pasticciosa
+            <div
+              class="align-middle py-2 px-2.5 text-xs rounded-xl text-gray-900 font-medium leading-snug tracking-wider uppercase bg-primary-200 hidden sm:inline-block shadow-sm"
+            >
+              B2B Portal
+            </div>
+          </h2>
+        </div>
+        <!-- search bar-->
+        <div class="container hidden relative left-0 z-50 md:flex h-auto">
+          <div class="relative flex items-center w-full h-full lg:w-64 group">
+            <div
+              class="absolute z-50 flex items-center justify-center w-auto h-10 p-3 pr-2 text-sm text-gray-500 uppercase cursor-pointer sm:hidden"
+            >
+              <svg
+                fill="none"
+                class="relative w-5 h-5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+            </div>
+            <svg
+              class="absolute left-0 z-20 hidden w-4 h-4 ml-4 text-gray-500 pointer-events-none fill-current group-hover:text-gray-400 sm:block"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"
+              ></path>
+            </svg>
+            <input
+              type="text"
+              class="block w-full py-1.5 pl-10 pr-4 leading-normal rounded-2xl focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 ring-opacity-90 bg-gray-100 dark:bg-gray-800 text-gray-400 aa-input"
+              placeholder="Search"
+            />
+            <div
+              class="absolute right-0 hidden h-auto px-2 py-1 mr-2 text-xs text-gray-400 border border-gray-300 rounded-2xl md:block"
+            >
+              +
+            </div>
+          </div>
+        </div>
+        <!-- buttons -->
+        <nav class="contents w-1/12">
+          <ul class="ml-4 flex items-center justify-end">
+            <li>
+              <span
+                class="block p-1 w-8 h-8 text-center text-white font-semibold rounded-full bg-slate-400 aspect-square"
+                >LP</span
+              >
+            </li>
+            <li class="ml-2 lg:ml-4 relative inline-block">
+              <a href="#" @click.prevent="auth.logout()">
+                <i class="h h-logout text-2xl text-red-600"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
-
-      <!-- phone number -->
-      <div class="ml-4 md:3/12 hidden sm:flex flex-col place-items-end">
-        <span class="font-bold md:text-xl">(+30) 216 800 22 62</span>
-        <span class="font-semibold text-sm text-gray-400">Υποστήριξη 24/7</span>
-      </div>
-
-      <!-- buttons -->
-      <nav class="contents w-1/12">
-        <ul class="ml-4 flex items-center justify-end">
-          <li class="ml-2 lg:ml-4 relative inline-block">
-            <a href="#" @click.prevent="auth.logout()">
-              <i class="h h-logout text-2xl text-red-600"></i>
-            </a>
-          </li>
-        </ul>
-      </nav>
     </div>
-
-    <hr />
   </header>
 </template>
 
--- src/client/src/components/reusables/navigation/navigation.vue
+++ src/client/src/components/reusables/navigation/navigation.vue
@@ -1,196 +1,102 @@
 <template>
-  <!-- component -->
-  <div @resize.window="watchScreen()" class="z-10">
-    <div class="flex min-h-screen antialiased text-gray-900">
-      <!-- Loading screen -->
-      <div
-        v-if="loading"
-        class="fixed inset-0 z-50 flex items-center justify-center text-2xl font-semibold text-white bg-primary-800"
-      >
-        Loading.....
-      </div>
-
-      <!-- Sidebar -->
-      <div class="flex flex-shrink-0 transition-all">
-        <div
-          v-if="isSidebarOpen"
-          @click="isSidebarOpen = false"
-          class="fixed inset-0 z-10 bg-black bg-opacity-50 lg:hidden"
-        ></div>
-        <div v-if="isSidebarOpen" class="fixed inset-y-0 z-10 w-16"></div>
-
-        <!-- Mobile bottom bar -->
-        <nav
-          aria-label="Options"
-          class="fixed inset-x-0 bottom-0 flex flex-row-reverse items-center justify-center gap-5 px-4 py-4 bg-white border-t border-slate-400 sm:hidden shadow-t rounded-t-3xl z-above backdrop-blur-lg backdrop-opacity-80 shadow-md"
-        >
-          <!-- Menu button -->
-          <button
-            @click="
-              isSidebarOpen && currentSidebarTab == 'linksTab'
-                ? (isSidebarOpen = false)
-                : (isSidebarOpen = true);
-              currentSidebarTab = 'linksTab';
-            "
-            class="p-2 transition-colors rounded-lg shadow-md hover:bg-primary-800 hover:text-white focus:outline-none focus:ring focus:ring-primary-600 focus:ring-offset-white focus:ring-offset-2 border border-slate-400 md:border-0"
-            :class="
-              isSidebarOpen && currentSidebarTab == 'linksTab'
-                ? 'text-white bg-primary-600'
-                : 'text-gray-500 bg-white'
-            "
+  <div class="flex">
+    <!-- Desktop Nav -->
+    <div class="relative hidden h-screen my-4 ml-4 mr-2 lg:block w-80">
+      <div class="h-full bg-white rounded-2xl shadow-lg">
+        <div class="flex items-center justify-center pt-6">
+          <a
+            href="#"
+            class="absolute -top-6 left-1/2 flex h-14 w-14 -translate-x-1/2 items-center justify-center rounded-3xl bg-primary-500 text-white shadow-lg transition duration-100 hover:bg-primary-600 sm:h-16 sm:w-16"
           >
-            <i class="h h-menu block text-xl mx-1.5"></i>
-          </button>
-
-          <!-- Help button -->
-          <button
-            @click="
-              isSidebarOpen && currentSidebarTab == 'helpTab'
-                ? (isSidebarOpen = false)
-                : (isSidebarOpen = true);
-              currentSidebarTab = 'helpTab';
-            "
-            class="p-2 transition-colors rounded-lg shadow-md hover:bg-primary-800 hover:text-white focus:outline-none focus:ring focus:ring-primary-600 focus:ring-offset-white focus:ring-offset-2 border border-slate-400 md:border-0"
-            :class="
-              isSidebarOpen && currentSidebarTab == 'helpTab'
-                ? 'text-white bg-primary-600'
-                : 'text-gray-500 bg-white'
-            "
-          >
-            <i class="h h-book block text-xl mx-1.5"></i>
-          </button>
-
-          <!-- Profile button -->
-          <RouterLink
-            type="button"
-            to="/dashboard/profile"
-            @click="
-              currentSidebarTab = 'accountTab';
-              isSidebarOpen = false;
-            "
-            class="p-2 transition-colors rounded-lg shadow-md hover:bg-primary-800 hover:text-white focus:outline-none focus:ring focus:ring-primary-600 focus:ring-offset-white focus:ring-offset-2 border border-slate-400 md:border-0"
-            :class="
-              isSidebarOpen && currentSidebarTab == 'accountTab'
-                ? 'text-white bg-primary-600'
-                : 'text-gray-500 bg-white'
-            "
-          >
-            <i class="h h-user block text-xl mx-1.5 translate-x-0.5"></i>
-          </RouterLink>
-        </nav>
-
-        <!-- Left mini bar -->
-        <nav
-          aria-label="Options"
-          class="z-20 flex-col items-center flex-shrink-0 hidden w-16 py-4 bg-white border-r-2 border-slate-300 shadow-md sm:flex rounded-tr-3xl rounded-br-3xl"
-        >
-          <div class="flex flex-col items-center flex-1 p-2 space-y-4">
-            <!-- Menu button -->
-            <button
-              @click="
-                isSidebarOpen && currentSidebarTab == 'linksTab'
-                  ? (isSidebarOpen = false)
-                  : (isSidebarOpen = true);
-                currentSidebarTab = 'linksTab';
-              "
-              class="p-2 transition-colors rounded-lg shadow-md hover:bg-primary-800 hover:text-white focus:outline-none focus:ring focus:ring-primary-600 focus:ring-offset-white focus:ring-offset-2 border border-slate-400"
-              :class="
-                isSidebarOpen && currentSidebarTab == 'linksTab'
-                  ? 'text-white bg-primary-600'
-                  : 'text-gray-500 bg-white'
-              "
+            <svg
+              class="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
             >
-              <i class="h h-menu block text-xl mx-1.5"></i>
-            </button>
-
-            <!-- help button-->
-            <button
-              @click="
-                isSidebarOpen && currentSidebarTab == 'helpTab'
-                  ? (isSidebarOpen = false)
-                  : (isSidebarOpen = true);
-                currentSidebarTab = 'helpTab';
-              "
-              class="p-2 transition-colors rounded-lg shadow-md hover:bg-primary-800 hover:text-white focus:outline-none focus:ring focus:ring-primary-600 focus:ring-offset-white focus:ring-offset-2 border border-slate-400"
-              :class="
-                isSidebarOpen && currentSidebarTab == 'helpTab'
-                  ? 'text-white bg-primary-600'
-                  : 'text-gray-500 bg-white'
-              "
-            >
-              <i class="h h-book block text-xl mx-1.5"></i>
-            </button>
-
-            <!-- account button-->
+              <path
+                fill-rule="evenodd"
+                d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+        </div>
+        <nav class="mt-6">
+          <div>
             <RouterLink
-              type="button"
-              to="/dashboard/profile"
-              @click="
-                currentSidebarTab = 'accountTab';
-                isSidebarOpen = false;
-              "
-              class="p-2 transition-colors rounded-lg shadow-md hover:bg-primary-800 hover:text-white focus:outline-none focus:ring focus:ring-primary-600 focus:ring-offset-white focus:ring-offset-2 border border-slate-400"
-              :class="
-                currentSidebarTab == 'accountTab'
-                  ? 'text-white bg-primary-600'
-                  : 'text-gray-500 bg-white'
-              "
+              v-for="item in header"
+              :key="item.title"
+              class="flex items-center justify-start w-full p-4 my-2 text-primary-500 font-bold transition-colors duration-200 border-r-4 bg-gradient-to-r from-white hover:border-primary-500 hover:to-primary-50 text-xl"
+              :to="item.hash"
             >
-              <i class="h h-user block text-xl mx-1 translate-x-0.5"></i>
+              <span class="text-left">
+                <i :class="`h h-${item.icon} text-2xl`"></i>
+              </span>
+              <span class="mx-4 text-sm font-normal tracking-wider">
+                {{ item.title }}
+              </span>
             </RouterLink>
           </div>
         </nav>
-
-        <div
-          v-if="isSidebarOpen"
-          class="fixed inset-y-0 left-0 z-10 flex-shrink-0 w-64 bg-white border-r-2 border-slate-300 shadow-lg sm:left-16 rounded-tr-3xl rounded-br-3xl sm:w-72 lg:static lg:w-64"
-        >
-          <nav
-            v-if="currentSidebarTab == 'linksTab'"
-            aria-label="Main"
-            class="flex flex-col h-full py-10"
-          >
-            <!-- Links -->
-            <div
-              class="flex-1 px-4 space-y-2 overflow-hidden hover:overflow-auto"
-            >
-              <RouterLink
-                :to="`/dashboard/${item.hash}`"
-                role="button"
-                v-for="item in header"
-                class="flex items-center space-x-2 text-primary-600 transition-colors rounded-lg group hover:bg-primary-500 hover:text-white active:text-white active:bg-primary-500"
-              >
-                <span aria-hidden="true" class="p-2 rounded-lg">
-                  <i
-                    class="h text-xl mx-1.5 my-0.5"
-                    :class="`h-${(item as any).icon}`"
-                  ></i>
-                </span>
-                <span>{{ item.title }}</span>
-              </RouterLink>
-            </div>
-          </nav>
-
-          <!-- <section v-if="currentSidebarTab == 'helpTab'" class="px-4 py-6">
-            <h2 class="text-xl">Account</h2>
-          </section> -->
-        </div>
-      </div>
-      <div class="px-1 md:px-3">
-        <!-- Main -->
-        <RouterView v-slot="{ Component, route }">
-          <TransitionExpand appear>
-            <component :is="Component" :key="route.path" />
-          </TransitionExpand>
-        </RouterView>
       </div>
     </div>
+    <div class="px-1 md:px-3 my-4 ml-2 w-full lg:w-9/12">
+      <!-- Main -->
+      <RouterView v-slot="{ Component, route }">
+        <TransitionExpand appear>
+          <component :is="Component" :key="route.path" />
+        </TransitionExpand>
+      </RouterView>
+    </div>
   </div>
+  <!-- Mobile Nav -->
+  <nav
+    class="fixed bottom-0 mx-auto sm:left-[calc((100vw-28rem)/2)] flex w-full justify-between gap-8 border-t bg-white px-10 py-4 text-xs sm:max-w-md sm:rounded-t-xl sm:border-transparent sm:text-sm sm:shadow-2xl lg:hidden [&>*:nth-child(3)]:mr-10"
+  >
+    <a
+      href="#"
+      class="absolute -top-7 left-1/2 flex h-14 w-14 -translate-x-1/2 items-center justify-center rounded-3xl bg-primary-500 text-white shadow-lg transition duration-100 hover:bg-primary-600 sm:-top-8 sm:h-16 sm:w-16"
+    >
+      <svg
+        class="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </a>
+    <RouterLink
+      v-for="item in header"
+      :key="item.hash"
+      :to="item.hash"
+      class="flex flex-col items-center gap-1 text-primary-500"
+    >
+      <i :class="`h h-${item.icon} text-xl text-slate-400`"></i>
+
+      <span
+        class="hidden sm:inline w-12 text-ellipsis overflow-hidden text-slate-400"
+        >{{ item.title }}</span
+      >
+    </RouterLink>
+  </nav>
 </template>
 
 <style scoped>
 .router-link-active {
-  @apply bg-primary-500 text-white;
+  @apply border-primary-500  to-primary-100;
+}
+.router-link-active span {
+  @apply font-bold text-primary-500;
+}
+
+.router-link-active i {
+  @apply text-primary-500;
 }
 </style>
 
@@ -205,8 +111,4 @@ const currentSidebarTab = ref("linksTab");
 const { header } = defineProps<{
   header: Array<{ title: string; icon: string; hash: string }>;
 }>();
-
-const watchScreen = () => {
-  if (window.innerWidth <= 1024) isSidebarOpen.value = false;
-};
 </script>
