b014746f3093dcbd85debd564140494c685c7afb
Theo Anastasiadis <theoanastasiadis98@gmail.com>
2024-02-26 22:21:16 +0200

    refactor: removing containers from individual views

--- src/client/src/components/user/deliveryTable.vue
+++ src/client/src/components/user/deliveryTable.vue
@@ -1,79 +1,68 @@
 <template>
-  <div
-    class="xt-card rounded-2xl p-6 sm:p-8 text-sm text-gray-900 xt-links-default bg-white"
-  >
-    <h3>Τοποθεσίες Παράδοσης</h3>
-    <p>
-      Παρακάτω βλέπετε τις τοποθεσίες παράδοσης που έχετε συνδέσει με το προφίλ
-      σας. Οι τοποθεσίες με την ένδειξη <strong>ΕΝΕΡΓΗ</strong> έχουν γίνει
-      αποδεκτές από τον διαχειριστή και μπορείτε να της επιλέξετε στην
-      δημιουργία παραγγελιών. Για να αιτηθείτε μια νέα τοποθεσία χρησιμοποιήστε
-      τον σύνδεσμο <strong>ΝΕΑ ΑΙΤΗΣΗ</strong>.
-    </p>
-    <div class="xt-row xt-row-2">
-      <div
-        v-for="location in locations"
-        :key="location.id"
-        class="w-full md:w-4/12"
-      >
-        <div class="card rounded-md p-3 bg-white shadow-md border">
-          <div class="xt-h5">{{ location.name }}</div>
-          <p class="text-lg">
-            <strong>{{ location.street }} {{ location.number }}</strong
-            ><br />{{ location.zip }}
-          </p>
-          <p class="mt-2">{{ location.details }}&nbsp;</p>
-          <Tooltip>
-            <template #trigger>
-              <div
-                class="my-5 mx-auto p-3 rounded-md text-center"
-                :class="{
-                  'bg-green-300': location.state === 'accepted',
-                  'bg-orange-300': location.state === 'requested',
-                }"
-                data-xt-tooltip
-              >
-                <a
-                  data-xt-tooltip-element
-                  class="decoration-none text-black cursor-pointer"
-                  >{{
-                    location.state === "requested" ? "Σε Αναμονή" : "Ενεργή"
-                  }}
-                  <i
-                    class="text-sm h"
-                    :class="{
-                      'h-clock-7': location.state === 'requested',
-                      'h-check-circle-1': location.state === 'accepted',
-                    }"
-                  ></i
-                ></a>
-              </div>
-            </template>
-            <template #content>
-              <div
-                class="py-2 px-2.5 text-xs xt-card bg-primary-400 rounded-md"
-              >
-                <p v-if="location.state === 'requested'">
-                  Οι τοποθεσίες παράδοσης που είναι σε αναμονή
-                  <strong>δεν μπορούν</strong> να χρησιμοποιηθούν σε
-                  παραγγελίες.
-                </p>
-                <p v-else>
-                  Οι ενεργές τοποθεσίες παράδοσης <strong>μπορούν</strong> να
-                  χρησιμοποιηθούν σε παραγγελίες.
-                </p>
-              </div>
-            </template>
-          </Tooltip>
-        </div>
+  <h3>Τοποθεσίες Παράδοσης</h3>
+  <p>
+    Παρακάτω βλέπετε τις τοποθεσίες παράδοσης που έχετε συνδέσει με το προφίλ
+    σας. Οι τοποθεσίες με την ένδειξη <strong>ΕΝΕΡΓΗ</strong> έχουν γίνει
+    αποδεκτές από τον διαχειριστή και μπορείτε να της επιλέξετε στην δημιουργία
+    παραγγελιών. Για να αιτηθείτε μια νέα τοποθεσία χρησιμοποιήστε τον σύνδεσμο
+    <strong>ΝΕΑ ΑΙΤΗΣΗ</strong>.
+  </p>
+  <div class="xt-row xt-row-2">
+    <div
+      v-for="location in locations"
+      :key="location.id"
+      class="w-full md:w-4/12"
+    >
+      <div class="card rounded-md p-3 bg-white shadow-md border">
+        <div class="xt-h5">{{ location.name }}</div>
+        <p class="text-lg">
+          <strong>{{ location.street }} {{ location.number }}</strong
+          ><br />{{ location.zip }}
+        </p>
+        <p class="mt-2">{{ location.details }}&nbsp;</p>
+        <Tooltip>
+          <template #trigger>
+            <div
+              class="my-5 mx-auto p-3 rounded-md text-center"
+              :class="{
+                'bg-green-300': location.state === 'accepted',
+                'bg-orange-300': location.state === 'requested',
+              }"
+              data-xt-tooltip
+            >
+              <a
+                data-xt-tooltip-element
+                class="decoration-none text-black cursor-pointer"
+                >{{ location.state === "requested" ? "Σε Αναμονή" : "Ενεργή" }}
+                <i
+                  class="text-sm h"
+                  :class="{
+                    'h-clock-7': location.state === 'requested',
+                    'h-check-circle-1': location.state === 'accepted',
+                  }"
+                ></i
+              ></a>
+            </div>
+          </template>
+          <template #content>
+            <div class="py-2 px-2.5 text-xs xt-card bg-primary-400 rounded-md">
+              <p v-if="location.state === 'requested'">
+                Οι τοποθεσίες παράδοσης που είναι σε αναμονή
+                <strong>δεν μπορούν</strong> να χρησιμοποιηθούν σε παραγγελίες.
+              </p>
+              <p v-else>
+                Οι ενεργές τοποθεσίες παράδοσης <strong>μπορούν</strong> να
+                χρησιμοποιηθούν σε παραγγελίες.
+              </p>
+            </div>
+          </template>
+        </Tooltip>
       </div>
-      <div class="w-full md:w-4/12">
-        <div class="card rounded-md border border-dashed border-gray-400 p-2">
-          <div class="xt-h5">Νέα Τοποθεσία</div>
-          <deliveryCreator
-            @delivery-requested="(l) => this.locations.push(l)"
-          />
-        </div>
+    </div>
+    <div class="w-full md:w-4/12">
+      <div class="card rounded-md border border-dashed border-gray-400 p-2">
+        <div class="xt-h5">Νέα Τοποθεσία</div>
+        <deliveryCreator @delivery-requested="(l) => this.locations.push(l)" />
       </div>
     </div>
   </div>
--- src/client/src/components/user/itemsViewer.vue
+++ src/client/src/components/user/itemsViewer.vue
@@ -1,42 +1,38 @@
 <template>
-  <div
-    class="xt-card rounded-2xl p-6 sm:p-8 text-sm text-gray-900 xt-links-default bg-white"
-  >
-    <Loader :loading="loading" />
-    <h3>Κατάλογος</h3>
-    <p>
-      Παρακάτω βλέπετε τα προϊόντα που έχουν ανατεθεί στον λογαριασμό σας
-      <strong>από τον διαχειριστή</strong>. Τα προϊόντα αυτά είναι είναι
-      διαθέσιμα για παραγγελία. Αν επιθυμείτε να προστεθεί στον κατάλογό σας
-      κάποιο νέο προϊόν παρακαλώ μην διστάσετε να επικοινωνήσετε με εμάς και θα
-      χαρούμε να δουλέψουμε μαζί για να την δημιουργία του προϊόντος που θα
-      καλήψει τις ανάγκες σας!
-    </p>
-    <div class="listing">
-      <div class="container px-0">
-        <div class="xt-row xt-row-4">
-          <div
-            class="w-full md:w-4/12 lg:w-3/12"
-            v-for="item in items"
-            :key="item.id"
-          >
-            <div class="listing-item">
-              <div class="xt-media-container bg-white pb-[75%]">
-                <img
-                  class="xt-media object-cover rounded-md transition duration-300 ease-in-out hover:scale-105"
-                  :src="item.image"
-                  loading="lazy"
-                  alt="product image"
-                />
+  <Loader :loading="loading" />
+  <h3>Κατάλογος</h3>
+  <p>
+    Παρακάτω βλέπετε τα προϊόντα που έχουν ανατεθεί στον λογαριασμό σας
+    <strong>από τον διαχειριστή</strong>. Τα προϊόντα αυτά είναι είναι διαθέσιμα
+    για παραγγελία. Αν επιθυμείτε να προστεθεί στον κατάλογό σας κάποιο νέο
+    προϊόν παρακαλώ μην διστάσετε να επικοινωνήσετε με εμάς και θα χαρούμε να
+    δουλέψουμε μαζί για να την δημιουργία του προϊόντος που θα καλήψει τις
+    ανάγκες σας!
+  </p>
+  <div class="listing">
+    <div class="container px-0">
+      <div class="xt-row xt-row-4">
+        <div
+          class="w-full md:w-4/12 lg:w-3/12"
+          v-for="item in items"
+          :key="item.id"
+        >
+          <div class="listing-item">
+            <div class="xt-media-container bg-white pb-[75%]">
+              <img
+                class="xt-media object-cover rounded-md transition duration-300 ease-in-out hover:scale-105"
+                :src="item.image"
+                loading="lazy"
+                alt="product image"
+              />
+            </div>
+            <div class="py-4">
+              <div class="xt-h6">{{ item.name }}</div>
+              <div class="-mt-2 text-base font-medium text-primary-600">
+                {{ item.price }} €
               </div>
-              <div class="py-4">
-                <div class="xt-h6">{{ item.name }}</div>
-                <div class="-mt-2 text-base font-medium text-primary-600">
-                  {{ item.price }} €
-                </div>
-                <div class="text-sm text-slate-500">
-                  {{ item.description }}
-                </div>
+              <div class="text-sm text-slate-500">
+                {{ item.description }}
               </div>
             </div>
           </div>
