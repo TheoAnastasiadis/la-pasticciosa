a89d8bd42d3c3508a2d355cafe65e88b4df648ef
Theo Anastasiadis <theoanastasiadis98@gmail.com>
2024-02-28 18:23:15 +0200

    feat(client): created testimonials component

---
+++ src/client/src/components/common/homepage/testimonials.vue
@@ -0,0 +1,128 @@
+<template>
+  <section class="px-4 pt-16 mx-auto max-w-7xl">
+    <div class="grid grid-cols-2 gap-10 text-center lg:grid-cols-8">
+      <div class="flex items-center justify-center">
+        <img
+          src="@/assets/logos/astir.png"
+          alt="Todoist Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="@/assets/logos/athenian-yachts.png"
+          alt="Slack Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="@/assets/logos/due_pentole.png"
+          alt="Typeform Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="@/assets/logos/electra_metropolis.png"
+          alt="Algolia Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="@/assets/logos/hams.jpg"
+          alt="Postcss Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="@/assets/logos/malconis.png"
+          alt="TailwindCSS Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="@/assets/logos/uva.png"
+          alt="Discord Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="@/assets/logos/electra_palace.png"
+          alt="Vimeo Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <!-- <div class="flex items-center justify-center">
+        <img
+          src="@/assets/logos/campfire.svg"
+          alt="Campfire Logo"
+          class="block object-contain h-12"
+        />
+      </div> 
+      <div class="flex items-center justify-center">
+        <img
+          src="/logos/android.svg"
+          alt="Android Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="/logos/atlassian.svg"
+          alt="Atlassian Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="/logos/apple.svg"
+          alt="Apple Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="/logos/google.svg"
+          alt="Google Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="/logos/zeplin.svg"
+          alt="Zeplin Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="/logos/visual-studio-code.svg"
+          alt="VSCode Logo"
+          class="block object-contain h-12"
+        />
+      </div>
+      <div class="flex items-center justify-center">
+        <img
+          src="/logos/ycombinator.svg"
+          alt="Ycombinator Logo"
+          class="block object-contain h-12"
+        />
+      </div> -->
+    </div>
+    <p class="mt-16 text-base font-medium text-center">
+      Δημιουργούμε στοιβαρές συνεργασίες πάνω από 10 χρόνια, βοηθώντας την
+      ανάπτηξη και επιτυχία των πάνω από
+      <a
+        href="#"
+        class="inline-flex items-center justify-center text-primary-600 hover:text-primary-700 underline"
+      >
+        100+ πελατών μας </a
+      >!
+    </p>
+  </section>
+</template>
