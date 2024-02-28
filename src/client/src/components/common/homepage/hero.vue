074734ce1e8bfe72584e7e8c2909749b7e71c22e
Theo Anastasiadis <theoanastasiadis98@gmail.com>
2024-02-28 18:22:35 +0200

    refactor(client): altered appearance of landing page hero component

--- src/client/src/components/common/homepage/hero.vue
+++ src/client/src/components/common/homepage/hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-screen min-h-screen flex flex-col-reverse md:flex-row justify-between items-center pt-20 md:pt-4 space-y-6 md:space-y-0"
+    class="w-screen min-h-screen flex flex-col-reverse md:flex-row justify-between items-center pt-20 md:pt-4 space-y-6 md:space-y-0 bg-[url(https://pluspng.com/img-png/sunrays-hd-png--1900.png)] bg-center bg-cover"
   >
     <div
       class="w-full md:w-3/12 flex flex-col md:min-h-screen justify-start md:justify-center space-y-5 pr-10 md:pr-0 py-16 md:py-0"
@@ -9,7 +9,7 @@
         href="#"
         v-for="plate in previewPlates"
         :key="plate.id"
-        class="block rounded-r-full h-28 shadow-2xl relative hover:-translate-y-5 transition-all cursor-pointer"
+        class="block rounded-r-full h-28 shadow-2xl relative hover:-translate-y-5 transition-all cursor-pointer border-1 border-slate-700 border-l-0"
         @click="
           previewPlates = previewPlates.map((p) => {
             p.id == plate.id ? (p.active = true) : (p.active = false);
@@ -18,7 +18,8 @@
         "
         :class="{
           '-translate-x-10': plate.id == 1,
-          'bg-primary-400 shadow-primary-400': plate.active,
+          'bg-primary-600 shadow-primary-600 border-primary-700 text-white':
+            plate.active,
           'bg-white': !plate.active,
         }"
       >
@@ -26,7 +27,7 @@
           class="absolute top-0 left-0 w-1/2 py-3 px-4 xl:px-2 xl:pl-4 h-full flex flex-row items-center"
           :class="{ 'translate-x-10': plate.id == 1 }"
         >
-          <p class="text-lg tracking-tight leading-tight text-start">
+          <p class="text-base tracking-tight leading-tight text-start">
             {{ plate.title }}
           </p>
         </div>
@@ -36,7 +37,7 @@
           class="absolute right-3 top-1.5 h-[6.25rem] aspect-square rounded-full border-2 object-cover"
           :class="{
             'border-white': plate.active,
-            'border-primary-400': !plate.active,
+            'border-green-500': !plate.active,
           }"
         />
       </button>
@@ -54,13 +55,13 @@
         class="absolute bottom-10 right-[15%] border-red-600 w-[100px] h-[100px]"
       ></div>
     </div>
-    <div class="w-full md:w-4/12 xl:w-5/12 pl-6 pr-10">
+    <div class="w-full md:w-5/12 xl:w-5/12 pl-6 pr-10">
       <sup
         class="md:hidden m-0 text-slate-500 bold tracking-widest text-center uppercase w-full inline-block"
         >La Pasticciosa</sup
       >
       <h1
-        class="normal-case xt-h1 text-5xl text-center md:text-left mt-0 md:mt-auto"
+        class="normal-case xt-h1 text-4xl xl:text-5xl text-center md:text-left mt-0 md:mt-auto"
       >
         Εξελίξτε το μενού σας με φρέσκα ζυμαρικά!
       </h1>
@@ -73,13 +74,14 @@
         class="flex flex-col xl:flex-row xl:space-x-5 space-y-5 xl:space-y-0 pt-10"
       >
         <button
-          class="bg-primary-500 text-base xl:text-lg rounded-2xl border-2 border-primary-600 text-white px-4 py-2 text-bold hover:shadow-2xl hover:scale-105 shadow-xl transition-transform"
+          class="bg-primary-600 text-base xl:text-lg rounded-md border-2 border-primary-700 text-white px-4 py-2 text-bold hover:shadow-2xl hover:scale-105 shadow-xl transition-transform"
+          @click="$emit('requestOffer')"
         >
-          Ζητήστε Προσφορά
+          ΖΗΤΗΣΤΕ ΠΡΟΣΦΟΡΑ
           <i class="h h-cake text-lg" />
         </button>
         <button
-          class="text-base xl:text-lg rounded-2xl border-2 border-slate-700 text-slate-800 px-4 py-2 hover:shadow-2xl hover:scale-105 shadow-xl transition-transform mt-0 ml-0"
+          class="text-base xl:text-lg rounded-md border-2 border-slate-700 text-slate-800 px-4 py-2 hover:shadow-2xl hover:scale-105 shadow-xl transition-transform mt-0 ml-0"
         >
           Μάθετε Περισσότερα
         </button>
@@ -113,25 +115,23 @@ const previewPlates = ref([
     id: 0,
     title: "Fagottini με κατσικίσιο τυρί",
     active: true,
-    image:
-      "https://www.the-pasta-project.com/wp-content/uploads/Homemade-fagottini-pasta-2-ways-8.jpg",
+    image: "./fagottini.png",
   },
   {
     id: 1,
     title: "Παπαρδέλες, κομμένες στο χέρι",
     active: false,
-    image:
-      "https://vikalinka.com/wp-content/uploads/2023/03/Tuscan-ragu-12-Edit.jpg",
+    image: "./papardelle.jpg",
   },
   {
     id: 2,
     title: "Ravioli με κολοκύθα και πικραμύγδαλο",
     active: false,
-    image:
-      "https://cucinapro.com/cdn/shop/products/RD-CCP-135-10-49_1800x1800.jpg?v=1654032923",
+    image: "./ravioli.jpg",
   },
 ]);
 const background = ref<HTMLDivElement>();
 
 defineExpose({ background });
+defineEmits(["requestOffer"]);
 </script>
